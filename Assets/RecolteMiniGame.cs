375f6bd5b075352215957b910f32483eb4d324d6
recrespe <remy.crespe@etu.uca.fr>
2020-02-12 14:08:03 +0100

    ajout d'un level

---
+++ Assets/RecolteMiniGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecolteMiniGame : MonoBehaviour
+{
+    [SerializeField] private int _idMiniGame;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            var script = GetComponent<PlayerController>();
+
+            script.AddRecolte(this);
+
+            gameObject.SetActive(false);
+        }
+    }
+}
